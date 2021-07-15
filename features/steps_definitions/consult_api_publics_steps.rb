4f7c0e82ce80c51aab30a5822b9a3f967876917c
Danilo Cavalcante <danilo.de.fc@gmail.com>
2021-07-15 08:29:09 -0300

    Alterando nome da variavel que sera usada na step definitions

--- features/steps_definitions/consult_api_publics_steps.rb
+++ features/steps_definitions/consult_api_publics_steps.rb
@@ -2,13 +2,13 @@ Dado('que consulto o endpoint que retorna todas as apis públicas') do
   @return_consult_api = @service.call(ConsultApiPublic).consult_apis_publics
 end
 
-Quando('validar o retorno do endpoint validando as apis que são do {string}') do |return_api|
+Quando('validar o retorno do endpoint validando as apis que são do {string}') do |desired_api|
   @return_apis_throught_text_sent = []
-  @return_consult_api['entries'].size.times{|count| @return_apis_throught_text_sent << @return_consult_api['entries'][count] if @return_consult_api['entries'][count]['API'].upcase.include? (return_api.upcase)}
+  @return_consult_api['entries'].size.times{|count| @return_apis_throught_text_sent << @return_consult_api['entries'][count] if @return_consult_api['entries'][count]['API'].upcase.include? (desired_api.upcase)}
 end
 
-Então('devo ter retornado apenas as apis que são do {string}') do |return_api|
+Então('devo ter retornado apenas as apis que são do {string}') do |desired_api|
   @return_apis_throught_text_sent.each do |returned_apis|
-    expect(returned_apis['API'].upcase).to include (return_api.upcase)
+    expect(returned_apis['API'].upcase).to include (desired_api.upcase)
   end
 end
